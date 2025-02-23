9fc93e43fd96336ef5c4e03446784e00aae08807
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-02-23 20:05:24 +0530

    tip for optimizing is-palindrome problem for SC

--- 5-is-palindrome.py
+++ 5-is-palindrome.py
@@ -5,22 +5,29 @@ class Solution1:
 
         for char in s_lower:
             if char.isalnum():
-                str_alphanumeric+=char
+                str_alphanumeric += char
             else:
                 continue
 
-        i, j = 0, len(str_alphanumeric)-1
-        
-        while(i < j):
+        i, j = 0, len(str_alphanumeric) - 1
+
+        while i < j:
             if str_alphanumeric[i] == str_alphanumeric[j]:
-                i+=1
-                j-=1
+                i += 1
+                j -= 1
                 continue
-            return False        
+            return False
         else:
             return True
-        
+
     # TC - O(n)
     # SC - O(n) - storing alphanumeric characters in a string
-        
+    """
+    To further reduce the space complexity, increment left pointer only when at left, 
+    non-aplhanumeric charcater found, decrement right pointer only when at right, non-alphanumeric
+    charcter found. Compare directly with lower() method (s[i].lower() == s[j].lower()) in
+    while instead of creating another string with all lower case characters
+    """
+
+
 # URL - https://neetcode.io/problems/is-palindrome
