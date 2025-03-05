0ee4a71bce99cea06529e0399dc3643e70bd40ea
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-03-05 08:39:58 +0530

    binary search (easy) implementation through iteration, recursion and built-in function

---
+++ 11-binary-search.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left_index = 0
+        right_index = len(nums) - 1
+
+        while left_index <= right_index:
+            mid_index = (left_index + right_index) // 2
+            mid_number = nums[mid_index]
+
+            if target == mid_number:
+                return mid_index
+            elif target < mid_number:
+                right_index = mid_index - 1
+            else:
+                left_index = mid_index + 1
+        return -1
+    # TC - O(logn)
+    # SC - O(1)
+    
+class Solution2:
+    def search(self, nums: List[int], target: int) -> int:
+        left_index =  0
+        right_index = len(nums) - 1
+        return self.binary_search(left_index, right_index, nums, target)
+    
+    def binary_search(self, left_index, right_index, nums, target):
+        if right_index < left_index:
+            return -1
+        
+        mid_index = (left_index+right_index)//2
+        mid_number = nums[mid_index]
+        
+        if target==mid_number:
+            return mid_index
+        elif target < mid_number:
+            right_index = mid_index - 1
+        else:
+            left_index = mid_index + 1
+            
+        return self.binary_search(left_index, right_index, nums, target)
+    
+    # TC - O(logn)
+    # SC - O(logn) -- Stack space
+            
